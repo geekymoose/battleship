dac392e692ab1da2e21b5d942bbc8dd18b8a23a0
geekymoose <constantin.masson@etu.univ-paris-diderot.fr>
2015-02-15 18:53:19 +0100

    Create model for Weapons (Only base foundation)

---
+++ src/com/battleship/models/weapons/ShotTypeMissile.java
@@ -0,0 +1,62 @@
+/*
+ * Class :      ShotTypeMissile
+ * Creation:    Feb 15, 2015
+ * Author :     Constantin MASSON
+ * 
+ */
+
+package com.battleship.models.weapons;
+
+
+
+
+
+/**
+ * <h1>ShotTypeMissile</h1>
+ * <p>
+ * public class ShotTypeMissile<br/>
+ * implements ShotType
+ * </p>
+ *
+ *
+ * 
+ * @date Feb 15, 2015
+ * @author  Constantin MASSON
+ * @author  Jessica FAVIN
+ * @author  Anthony CHAFFOT
+ */
+public class ShotTypeMissile implements ShotType{
+    //**************************************************************************
+    // Constants - Variables
+    //**************************************************************************
+    
+    
+    
+    
+    
+
+    //**************************************************************************
+    // Constructor - Initialization
+    //**************************************************************************
+    public ShotTypeMissile(){
+    
+    }
+    
+    
+    
+    
+    
+
+    //**************************************************************************
+    // Functions
+    //**************************************************************************
+    
+    
+    
+    
+    
+
+    //**************************************************************************
+    // Getters - Setters
+    //**************************************************************************
+}
---
+++ src/com/battleship/models/weapons/ShotTypeNuclearBomb.java
@@ -0,0 +1,62 @@
+/*
+ * Class :      ShotTypeNuclearBomb
+ * Creation:    Feb 15, 2015
+ * Author :     Constantin MASSON
+ * 
+ */
+
+package com.battleship.models.weapons;
+
+
+
+
+
+/**
+ * <h1>ShotTypeNuclearBomb</h1>
+ * <p>
+ * public class ShotTypeNuclearBomb<br/>
+ * implements ShotType
+ * </p>
+ *
+ *
+ * 
+ * @date Feb 15, 2015
+ * @author  Constantin MASSON
+ * @author  Jessica FAVIN
+ * @author  Anthony CHAFFOT
+ */
+public class ShotTypeNuclearBomb implements ShotType{
+    //**************************************************************************
+    // Constants - Variables
+    //**************************************************************************
+    
+    
+    
+    
+    
+
+    //**************************************************************************
+    // Constructor - Initialization
+    //**************************************************************************
+    public ShotTypeNuclearBomb(){
+    
+    }
+    
+    
+    
+    
+    
+
+    //**************************************************************************
+    // Functions
+    //**************************************************************************
+    
+    
+    
+    
+    
+
+    //**************************************************************************
+    // Getters - Setters
+    //**************************************************************************
+}
