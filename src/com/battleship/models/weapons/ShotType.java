00de196782815f8dc8e258f7bcdb06d35e25cfa2
geekyMoose <constantin.grinda@gmail.com>
2015-02-15 18:53:19 +0100

    Create model for Weapons (Only base foundation)

---
+++ src/com/battleship/models/weapons/ShotType.java
@@ -0,0 +1,27 @@
+/*
+ * Class :      ShotType
+ * Creation:    Feb 15, 2015
+ * Author :     Constantin MASSON
+ * 
+ */
+
+package com.battleship.models.weapons;
+
+
+
+
+
+/**
+ * <h1>ShotType</h1>
+ * <p>public interface ShotType</p>
+ *
+ *
+ * 
+ * @date Feb 15, 2015
+ * @author  Constantin MASSON
+ * @author  Jessica FAVIN
+ * @author  Anthony CHAFFOT
+ */
+public interface ShotType {
+    
+}
---
+++ src/com/battleship/models/weapons/ShotTypeBomb.java
@@ -0,0 +1,62 @@
+/*
+ * Class :      ShotTypeBomb
+ * Creation:    Feb 15, 2015
+ * Author :     Constantin MASSON
+ * 
+ */
+
+package com.battleship.models.weapons;
+
+
+
+
+
+/**
+ * <h1>ShotTypeBomb</h1>
+ * <p>
+ * public class ShotTypeBomb<br/>
+ * implements ShotType
+ * </p>
+ *
+ *
+ * 
+ * @date Feb 15, 2015
+ * @author  Constantin MASSON
+ * @author  Jessica FAVIN
+ * @author  Anthony CHAFFOT
+ */
+public class ShotTypeBomb implements ShotType{
+    //**************************************************************************
+    // Constants - Variables
+    //**************************************************************************
+    
+    
+    
+    
+    
+
+    //**************************************************************************
+    // Constructor - Initialization
+    //**************************************************************************
+    public ShotTypeBomb(){
+    
+    }
+    
+    
+    
+    
+    
+
+    //**************************************************************************
+    // Functions
+    //**************************************************************************
+    
+    
+    
+    
+    
+
+    //**************************************************************************
+    // Getters - Setters
+    //**************************************************************************
+}
