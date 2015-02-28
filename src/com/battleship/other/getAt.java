28a62bb284d8d2de833249840571d50d2674f7f3
Jessica Favin <jessica00228@hotmail.fr>
2015-02-28 11:33:43 +0100

    Work on getAt

---
+++ src/com/battleship/other/getAt.java
@@ -0,0 +1,135 @@
+/*
+ * File :       getAt
+ * Creation :   28 févr. 2015
+ * Author :     Anthony CHAFFOT
+ * Author :     Jessica Favin
+ * Author :     Constantin MASSON
+ * 
+ * Projet computer science Java L2 - Battleship
+ * 
+ */
+package com.battleship.other;
+
+import com.battleship.models.game.FleetGridSquare;
+import com.battleship.models.sprites.Boat;
+import java.awt.Point;
+
+/**
+ * <h1>getAt</h1>
+ * <p>
+ * public class getAt<br/>
+ * </p>
+ *
+ * <p>
+ * Description</p>
+ *
+ * @date 28 févr. 2015
+ * @author Anthony CHAFFOT
+ * @author Jessica FAVIN
+ * @author Contsantin MASSON
+ */
+public class getAt {
+/////////////////////////////////////////////////////////////////////////
+
+    public Point getUpperLeftCoordinates(Boat pBoat) {
+        if (pBoat.grid instanceof FleetGridSquare) {
+            return getUpperLeftCoordinatesSquare(Boat pBoat
+          
+            );
+      }
+      else{
+         return getUpperLeftCoordinatesHexagon(Boat pBoat
+        
+    
+
+    );
+      }
+   }
+   
+   public Point getCenterImg(Boat pBoat) {
+        if (pBoat.grid instanceof FleetGridSquare) {
+            return getCenterImgSquare(Boat pBoat
+          
+            );
+      }
+      else{
+         return getCenterImgHexagon(Boat pBoat
+        
+    
+
+    );
+      }
+   }
+   
+   public Point getFrontPositionImg(Boat pBoat) {
+        if (pBoat.grid instanceof FleetGridSquare) {
+            return getFrontPositionImgSquare(Boat pBoat
+          
+            );
+      }
+      else{
+         return getFrontPositionImgHexagon(Boat pBoat
+        
+    
+
+    );
+      }
+   }
+///////////////////////////////////////////////////////////////////////////
+   public Point getCenterImgSquare(Boat pBoat) {
+        //vertical boat
+        if (pBoat.getOrientation() == 1) {
+            return new Point(pBoat.getFrontPosition().getPosX() * 40 + 20,
+                    pBoat.getFrontPosition().getPosY() * 40 + pBoat.tabCompartments.length * 40 / 2);
+        } //horizontal boat
+        else {
+            return new Point(pBoat.getFrontPosition().getPosX() * 40 + pBoat.tabCompartments.length * 40 / 2,
+                    pBoat.getFrontPosition().getPosY() * 40 + 20);
+        }
+    }
+
+    public Point getUpperLeftCoordinatesSquare(Boat pBoat) {
+        return new Point(pBoat.getFrontPosition().getPosX() * 40,
+                pBoat.getFrontPosition().getPosY() * 40);
+    }
+
+    public Point getFrontPositionImgSquare(Boat pBoat) {
+        return new Point(pBoat.getFrontPosition().getPosX() * 40 + 20,
+                pBoat.getFrontPosition().getPosY() * 40 + 20);
+    }
+
+   ///////////////////////////////////////////////////////////////////////////////////////////
+    //This section is completely effed up cause I don't know how to calculate the coordinates//
+    ///////////////////////////////////////////////////////////////////////////////////////////
+    public Point getCenterImgHexagon(Boat pBoat) {
+        //vertical boat
+        if (pBoat.getOrientation() == 1) {
+            return new Point(pBoat.getFrontPosition().getPosX() + 20,
+                    pBoat.getFrontPosition().getPosY() + pBoat.tabCompartments.length * 40 / 2);
+        } //horizontal boat
+        else {
+            //set to the right
+            if (pBoat.getOrientation() == 3) {
+                return new Point(pBoat.getFrontPosition().getPosX() + pBoat.tabCompartments.length * 40 / 2,
+                        pBoat.getFrontPosition().getPosY() + 20);
+            } //set to the left
+            else {
+                return new Point(pBoat.getFrontPosition().getPosX() - pBoat.tabCompartments.length * 40 / 2,
+                        pBoat.getFrontPosition().getPosY() + 20);
+            }
+        }
+    }
+
+    public Point getUpperLeftCoordinatesHexagon(Boat pBoat) {
+        if (pBoat.getOrientation() == 4) {
+            /*
+             getFrontPosition en cas de bateau en diagonal vers la gauche
+             retourne les coordonées de la boxmap en bas à gaueche?
+             return new Point(??,??);
+             */
+        } else {
+            return new Point(pBoat.getFrontPosition().getPosX() * 40,
+                    pBoat.getFrontPosition().getPosY() * 40);
+        }
+    }
+}
